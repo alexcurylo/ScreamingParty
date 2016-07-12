fc27b444d5bb635bb061c2af9a099702e0295259
Alex Curylo <alex@alexcurylo.com>
2016-07-12 16:38:52 -0700

    Add RawRepresentable-Equatable

---
+++ ScreamingParty/ScreamingParty/Classes/RawRepresentable-Equatable.swift
@@ -0,0 +1,19 @@
+//
+//  RawRepresentable-Equatable.swift
+//  ScreamingParty
+//
+//  Created by Alex Curylo on 2016-07-10.
+//  Copyright © 2016 Trollwerks Inc. All rights reserved.
+//
+//  Source:
+//  https://gist.github.com/apocolipse/1b3f5c1ba70148b5d0d62af8a93be438
+
+import Foundation
+
+func ==<U: Equatable, T: protocol<RawRepresentable, Equatable> where T.RawValue == U>(lhs: U, rhs: T) -> Bool {
+    return lhs == rhs.rawValue
+}
+
+func !=<U: Equatable, T: protocol<RawRepresentable, Equatable> where T.RawValue == U>(lhs: U, rhs: T) -> Bool {
+    return lhs != rhs.rawValue
+}
